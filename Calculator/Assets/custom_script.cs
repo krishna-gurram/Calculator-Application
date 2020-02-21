c57ce6bf9ad79c1d248a215ff5cca018bf0068b1
krishna-gurram <krishnaa.gsk@gmail.com>
2020-02-20 23:18:08 -0700

    Initial Commit

---
+++ Calculator/Assets/custom_script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class custom_script : MonoBehaviour
+{
+
+    public InputField number1, number2;
+    public Text result;
+
+    private int num1,num2;
+
+    public void onAdd() {
+    	
+    	 num1 = int.Parse(number1.text);
+    	 num2 = int.Parse(number2.text);
+
+    	 result.text = "Result : " + (num1 + num2).ToString();
+    	
+    }
+
+    public void onMultiply()
+    {
+
+        num1 = int.Parse(number1.text);
+        num2 = int.Parse(number2.text);
+
+        result.text = "Result : " + (num1 * num2).ToString();
+
+    }
+}
